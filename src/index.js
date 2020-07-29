b7c265fb1cbc7aadece7e297e966bc8ef9848ffb
SheillyR <sheillyrolo@gmail.com>
2020-07-29 13:31:12 -0500

    Cambio a arrow functions en JavaScript, mejora de nombres de variables, comentarios

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import validator from './validator.js';
 
 let slideIndex = 0
 let slides = document.getElementsByClassName("slides")
+
+// Arrow function que muestra las imágenes cada 2 segundos en la ventana Welcome.
+
 let carousel = () => {
     let i
     for(i=0; i < slides.length; i++){
@@ -14,118 +17,125 @@ let carousel = () => {
     slides[slideIndex-1].classList.remove('hide')
     setTimeout(carousel,2000) // Change image every 2 seconds
 }
+
 window.addEventListener('load', carousel)
 
 let nowDonate = document.getElementById('nowdonate')
-let welcomePage = document.getElementById('welcome')
-let registerPage = document.getElementById('register')
+let welcomeWindow = document.getElementById('welcome')
+let registerWindow = document.getElementById('register')
+
+// Arrow function con el evento click, que oculta la ventana de bienvenida y muestra 
+// la ventana de registro.
 
 nowDonate.addEventListener('click', () => {
-    welcomePage.classList.add('hide')
-    welcomePage.classList.remove('display')
+    welcomeWindow.classList.add('hide')
+    welcomeWindow.classList.remove('display')
 
-    registerPage.classList.add('display')
-    registerPage.classList.remove('hide')
+    registerWindow.classList.add('display')
+    registerWindow.classList.remove('hide')
 })
 
-let oneTimeButton = document.getElementById('onetime');
-oneTimeButton.addEventListener('click',oneTime);
-let monthlyButton = document.getElementById('monthly');
-monthlyButton.addEventListener('click',monthly);
-let next = document.getElementById('next');
-next.addEventListener('click',nextPage);
-let donate = document.getElementById('donate');
+let oneTimeButton = document.getElementById('onetime')
+let monthlyButton = document.getElementById('monthly')
+let oneTimeAmount = document.getElementById('onetimeamount')
+let monthlyAmount = document.getElementById('monthlyamount')
 
-let back =  document.getElementById('back');
-back.addEventListener('click', tryAgain);
-let donateAgain = document.getElementById('donateagain');
-donateAgain.addEventListener('click', payAgain);
-let creditCardNumber;
+// Arrow functions con el evento click, muestra u oculta los montos de los botones 
+// Única y Mensual de la ventana de registro.
 
-let oneTimeAmount = document.getElementById('onetimeamount');
-let monthlyAmount = document.getElementById('monthlyamount');
-
-let paymentPage = document.getElementById('payment');
-let valid = document.getElementById('validation');
-let invalid = document.getElementById('invalidation');
-
-
-
-
-
-
-function oneTime(){
+oneTimeButton.addEventListener('click', () => {
     
-    monthlyAmount.classList.add('hide');
-    monthlyAmount.classList.remove('display');
+    monthlyAmount.classList.add('hide')
+    monthlyAmount.classList.remove('display')
     
-    oneTimeAmount.classList.add('display');
-    oneTimeAmount.classList.remove('hide');
-}
+    oneTimeAmount.classList.add('display')
+    oneTimeAmount.classList.remove('hide')
+})
 
+monthlyButton.addEventListener('click', () => {
 
-function monthly(){
-
-    oneTimeAmount.classList.add('hide');
+    oneTimeAmount.classList.add('hide')
     oneTimeAmount.classList.remove('display');
 
-    monthlyAmount.classList.add('display');
-    monthlyAmount.classList.remove('hide');    
-}
+    monthlyAmount.classList.add('display')
+    monthlyAmount.classList.remove('hide') 
+})
 
+let nextButton = document.getElementById('next')
+let paymentWindow = document.getElementById('payment')
 
-function nextPage(){
+// Arrow function con el evento click, para el botón siguiente que oculta la ventana 
+// de registro y muestra la ventana de pago con tarjeta de crédito.
 
-    registerPage.classList.add('hide');
-    registerPage.classList.remove('display');
+nextButton.addEventListener('click', () => {
+    registerWindow.classList.add('hide')
+    registerWindow.classList.remove('display')
 
-    paymentPage.classList.add('display');
-    paymentPage.classList.remove('hide');
+    paymentWindow.classList.add('display')
+    paymentWindow.classList.remove('hide')
+})
 
+let donate = document.getElementById('donate')
+let validityResult = document.getElementById('validation')
+let invalidityResult = document.getElementById('invalidation')
 
-}
+// Arrow function con el evento click, para el botón donar que va capturar el valor del
+// número de la tarjeta de crédito, y aplicar los métodos isvalid y maskify del objeto
+// validator, además de ocultar la ventana de pago y mostrar la ventana de resultado 
+// válido e inválido dependiendo del resultado. 
 
 donate.addEventListener('click', () => {
-    creditCardNumber = document.getElementById('cardnumber').value;
-    let mask = validator.maskify(creditCardNumber);
+    let creditCardNumber = document.getElementById('cardnumber').value
+    let mask = validator.maskify(creditCardNumber)
+    
     if(validator.isValid(creditCardNumber) === true){
 
-        paymentPage.classList.add('hide');
-        paymentPage.classList.remove('display');
+        paymentWindow.classList.add('hide')
+        paymentWindow.classList.remove('display')
   
-        valid.classList.add('display');
-        valid.classList.remove('hide');
+        validityResult.classList.add('display')
+        validityResult.classList.remove('hide')
 
-        document.getElementById('message').innerHTML = mask;
+        document.getElementById('message').innerHTML = mask
 
     }else{
 
-        paymentPage.classList.add('hide');
-        paymentPage.classList.remove('display');
+        paymentWindow.classList.add('hide')
+        paymentWindow.classList.remove('display')
 
-        invalid.classList.add('display');
-        invalid.classList.remove('hide');
+        invalidityResult.classList.add('display')
+        invalidityResult.classList.remove('hide')
 
-        document.getElementById('messagemask').innerHTML = mask;
-
+        document.getElementById('messagemask').innerHTML = mask
     }
-}
-)
+})
 
-function tryAgain(){
-    invalid.classList.add('hide');
-    invalid.classList.remove('display');
+let donateAgain = document.getElementById('donateagain')
 
-    paymentPage.classList.add('display');
-    paymentPage.classList.remove('hide');
-}
+// Arrow function con evento click, para el botón donar otra vez, que muestra
+// la página de registro y oculta la del resultado válido.
 
-function payAgain(){
-    valid.classList.add('hide');
-    valid.classList.remove('display');
+donateAgain.addEventListener('click', () => {
+    validityResult.classList.add('hide')
+    validityResult.classList.remove('display')
 
-    registerPage.classList.add('display');
-    registerPage.classList.remove('hide');
-}
+    registerWindow.classList.add('display')
+    registerWindow.classList.remove('hide')
+})
+
+let returnWindow =  document.getElementById('back')
+
+// Arrow function con evento click, para el botón de volver a intentar que permite
+// retornar a la página donde se selecciona el monto a donar, y oculta la del 
+// resultado inválido.
+
+returnWindow.addEventListener('click', () => {
+    invalidityResult.classList.add('hide');
+    invalidityResult.classList.remove('display');
+
+    paymentWindow.classList.add('display');
+    paymentWindow.classList.remove('hide');
+})
+
 
 
