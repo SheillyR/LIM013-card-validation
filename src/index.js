d0e0990bd43d13aa97aaf6916bcda319ab7b0c38
SheillyR <sheillyrolo@gmail.com>
2020-07-30 01:48:18 -0500

    Ventana payment que muestra el monto elegido y botón donateAagain actualizado

--- src/index.js
+++ src/index.js
@@ -26,8 +26,8 @@ let registerWindow = document.getElementById('register')
 let monthlyButton = document.getElementById('monthly')
 
 
-// Arrow function con el evento click, que oculta la ventana de bienvenida y muestra 
-// la ventana de registro.
+/* Arrow function con el evento click, que oculta la ventana de bienvenida y muestra 
+   la ventana de registro. */
 
 nowDonate.addEventListener('click', () => {
     welcomeWindow.classList.add('hide')
@@ -37,14 +37,19 @@ nowDonate.addEventListener('click', () => {
     registerWindow.classList.remove('hide')
 
     monthlyButton.focus()
+
 })
 
 let oneTimeButton = document.getElementById('onetime')
 let oneTimeAmount = document.getElementById('onetimeamount')
 let monthlyAmount = document.getElementById('monthlyamount')
+let amountOneTime = document.getElementsByName('donation-one-time')
+let amountMonthly = document.getElementsByName('donation-monthly')
+let otherAmount = document.getElementById('otheramount')
 
-// Arrow functions con el evento click, muestra u oculta los montos de los botones 
-// Única y Mensual de la ventana de registro.
+
+/* Arrow functions con el evento click, muestra u oculta los montos de los botones 
+   Única y Mensual de la ventana de registro. */
 
 oneTimeButton.addEventListener('click', () => {
     
@@ -53,6 +58,13 @@ oneTimeButton.addEventListener('click', () => {
     
     oneTimeAmount.classList.add('display')
     oneTimeAmount.classList.remove('hide')
+
+    for(let k = 0; k < amountMonthly.length; k++){
+        if(amountMonthly[k].checked ){
+            amountMonthly[k].checked = false
+        }
+    }
+    otherAmount.value = ''
 })
 
 monthlyButton.addEventListener('click', () => {
@@ -62,13 +74,36 @@ monthlyButton.addEventListener('click', () => {
 
     monthlyAmount.classList.add('display')
     monthlyAmount.classList.remove('hide') 
+
+    for(let j = 0; j < amountOneTime.length; j++){
+        if(amountOneTime[j].checked){
+            amountOneTime[j].checked = false
+        }
+    }
+    otherAmount.value = ''
 })
+
+/* if(otherAmount != ''){
+    for(let j = 0; j < amountOneTime.length; j++){
+        if(amountOneTime[j].checked){
+            amountOneTime[j].checked = false
+        }
+    }
+    for(let k = 0; k < amountMonthly.length; k++){
+        if(amountMonthly[k].checked ){
+            amountMonthly[k].checked = false
+        }
+    }
+}
+*/
 
 let nextButton = document.getElementById('next')
 let paymentWindow = document.getElementById('payment')
+let options = document.getElementsByName('options')
+let showAmount = document.getElementById('amount')
 
-// Arrow function con el evento click, para el botón siguiente que oculta la ventana 
-// de registro y muestra la ventana de pago con tarjeta de crédito.
+/* Arrow function con el evento click, para el botón siguiente que oculta la ventana 
+   de registro y muestra la ventana de pago con tarjeta de crédito. */
 
 nextButton.addEventListener('click', () => {
     registerWindow.classList.add('hide')
@@ -76,6 +111,32 @@ nextButton.addEventListener('click', () => {
 
     paymentWindow.classList.add('display')
     paymentWindow.classList.remove('hide')
+    
+    if (otherAmount.value !== ''){
+        showAmount.innerHTML = `<p>S/. ${otherAmount.value}</p>`
+    }
+    
+    for (let i = 0; i < options.length; i++){
+        if(options[i].selectedIndex = '0'){
+            for(let j = 0; j < amountOneTime.length; j++){
+                if(amountOneTime[j].checked){
+                    showAmount.innerHTML= `<p>S/. ${amountOneTime[j].value}.00</p>`
+                }
+                amountOneTime[j].checked = false
+            }
+
+        }
+        if(options[i].selectedIndex = '1'){
+            for(let k = 0; k < amountMonthly.length; k++){
+                if(amountMonthly[k].checked ){
+                    showAmount.innerHTML = `<p>S/. ${amountMonthly[k].value}.00</p>`
+                }
+                amountMonthly[k].checked = false
+            }
+
+        } 
+
+    }
 })
 
 let donate = document.getElementById('donate')
@@ -84,10 +145,10 @@ let invalidityResult = document.getElementById('invalidation')
 let elementValidityMessage = document.getElementById('validity-message')
 let elementInvalidityMessage = document.getElementById('invalidity-message')
 
-// Arrow function con el evento click, para el botón donar que va capturar el valor del
-// número de la tarjeta de crédito, y aplicar los métodos isvalid y maskify del objeto
-// validator, además de ocultar la ventana de pago y mostrar la ventana de resultado 
-// válido e inválido dependiendo del resultado. 
+/* Arrow function con el evento click, para el botón donar que va capturar el valor del
+   número de la tarjeta de crédito, y aplicar los métodos isvalid y maskify del objeto
+   validator, además de ocultar la ventana de pago y mostrar la ventana de resultado 
+   válido e inválido dependiendo del resultado. */
 
 donate.addEventListener('click', () => {
 
@@ -123,8 +184,8 @@ donate.addEventListener('click', () => {
 
 let donateAgain = document.getElementById('donateagain')
 
-// Arrow function con evento click, para el botón donar otra vez, que muestra
-// la página de registro y oculta la del resultado válido.
+/* Arrow function con evento click, para el botón donar otra vez, que muestra
+   la página de registro y oculta la del resultado válido. */
 
 donateAgain.addEventListener('click', () => {
     validityResult.classList.add('hide')
@@ -133,14 +194,22 @@ donateAgain.addEventListener('click', () => {
     registerWindow.classList.add('display')
     registerWindow.classList.remove('hide')
 
+    monthlyAmount.classList.remove('hide') 
+    monthlyAmount.classList.add('display')
+
+    oneTimeAmount.classList.add('hide')
+    oneTimeAmount.classList.remove('display');
+    
     monthlyButton.focus()
+
+    otherAmount.value = ''
 })
 
 let returnWindow =  document.getElementById('back')
 
-// Arrow function con evento click, para el botón de volver a intentar que permite
-// retornar a la página donde se selecciona el monto a donar, y oculta la del 
-// resultado inválido.
+/* Arrow function con evento click, para el botón de volver a intentar que permite
+   retornar a la página donde se selecciona el monto a donar, y oculta la del 
+   resultado inválido. */
 
 returnWindow.addEventListener('click', () => {
     invalidityResult.classList.add('hide');
