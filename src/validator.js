fd6447fb3a0c65501c2c06d17df8524067683d0e
SheillyR <sheillyrolo@gmail.com>
2020-08-05 10:13:55 -0500

    Se mejoro objeto validator

--- src/validator.js
+++ src/validator.js
@@ -1,37 +1,36 @@
 const validator = {
 
     isValid: function (creditCardNumber){
-    creditCardNumber = creditCardNumber.split(' ').join("")
+    creditCardNumber = creditCardNumber.split('').join('')
     if (parseInt(creditCardNumber) <= 0 || creditCardNumber.length > 16) {
-        return false;
+        return false
     }
-    var carray = new Array();
-    for (var j = 0; j < creditCardNumber.length; j++) {
+    let carray = new Array()
+    for (let j = 0; j < creditCardNumber.length; j++) {
         carray[carray.length] = creditCardNumber.charCodeAt(j) - 48;
     }
-    carray.reverse();
-    var sum = 0;
-    for (var i = 0; i < carray.length; i++) {
-        var tmp = carray[i];
+    carray.reverse()
+    let sum = 0
+    for (let i = 0; i < carray.length; i++) {
+        let tmp = carray[i]
         if ((i % 2) != 0) {
-            tmp *= 2;
+            tmp *= 2
             if (tmp > 9) {
-                tmp -= 9;
+                tmp -= 9
             }
         }
         sum += tmp;
     }
     if ((sum % 10) == 0){
-        return true;
-    }
-    else{
-        return false;
+        return true
+    }else{
+        return false
     }
 },
 
 maskify: function(number){
     let hash = number.replace(/.(?=.{4,}$)/g, '#');
-    return hash;
+    return hash
 }
 }
 
